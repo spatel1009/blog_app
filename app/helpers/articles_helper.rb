8d17f02b5f12dd0f94dcc354de86eaa67a7a390d
spatel1009 <spatel1009@gmail.com>
2019-06-05 17:29:52 -0400

    spread logic to view helper and refactored the views witha partial

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+
+  def index_content
+    if @articles.empty? 
+      default = 'No Articles Created'
+      content_tag(:h1, default, id: 'no-articles')
+    else
+      render partial: 'list_articles'
+    end
+  end
+
 end
