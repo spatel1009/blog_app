ff7dbe1ab24a8dcdd4d8865b6653fbd6a8432991
spatel1009 <spatel1009@gmail.com>
2019-06-06 17:49:57 -0400

    set up user sign up, edited nav conditional link render based on log in status

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature 'Listing Articles' do
+
+  scenario 'with valid credentials' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: 'test@test.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
+
+    expect(page).to have_content('You have signed up successfully.')
+  end
+
+  scenario 'with invalid credentials' do
+    visit '/'
+    click_link 'Sign up'
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_button 'Sign up'
+
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+
+end
